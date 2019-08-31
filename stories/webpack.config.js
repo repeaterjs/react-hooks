258a5de53878e8ddb1c9960e98000a1efa3889d9
Brian Kim <briankimpossible@gmail.com>
2019-08-31 18:05:35 -0400

    lint stories

--- stories/webpack.config.js
+++ stories/webpack.config.js
@@ -1,8 +1,8 @@
 module.exports = function({ config }) {
   config.module.rules.push({
     test: /\.stories\.jsx?$/,
-    loaders: [require.resolve('@storybook/source-loader')],
-    enforce: 'pre',
+    loaders: [require.resolve("@storybook/source-loader")],
+    enforce: "pre",
   });
 
   return config;
